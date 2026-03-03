f9d6dee927e579b828943a11a0824b8c8379dbbb
김도연 <kimdoyeon0113@naver.com>
2026-03-03 16:32:19 +0900

    feat: 타입 별칭과 인덱스 시그니처

---
+++ src/chapter4.js
@@ -0,0 +1,22 @@
+// 타입 별칭이 사라짐
+var user = {
+  id: 1,
+  name: "kim",
+  nickname: "hido",
+  birth: "2003",
+  bio: "안녕",
+  location: "서울시",
+};
+var user2 = {
+  id: 1,
+  name: "Lee",
+  nickname: "garnet",
+  birth: "2002",
+  bio: "hi",
+  location: "경기도",
+};
+// 불가
+// type User = {
+//   height: number;
+// };
+var test = function () {};
